d1143b4c89a48b5cbdefc7b65898b86d575e3fa5
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-07 16:27:14 +0300

    задание 19

---
+++ PrG1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine());
+if (a < 100000 && a > 10000)
+{
+  int a1 = a/10000;
+  int a2 = a/1000%10;
+  int a5 = a%10;
+  int a4 = a%100/10;
+  if (a1 == a5 && a2 == a4)
+  {
+    Console.WriteLine($"{a} -> да");
+  }
+  else 
+  {
+    Console.WriteLine($"{a} -> нет");
+  }
+}
+else
+{
+    Console.WriteLine("Введите пятизначное число");
+}
+
